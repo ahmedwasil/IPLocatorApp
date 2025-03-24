f3d5d5ddb937fa9131665eab077f1f9c99c2857a
Wasil Ahmed <ahmed.wasil@gmail.com>
2025-03-24 04:55:52 +0000

    Initial Commit

---
+++ IPLocatorApp/IPLocatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPLocatorAppApp.swift
+//  IPLocatorApp
+//
+//  Created by Wasil Ahmed on 24/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct IPLocatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IPLocatorAppTests/IPLocatorAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  IPLocatorAppTests.swift
+//  IPLocatorAppTests
+//
+//  Created by Wasil Ahmed on 24/03/2025.
+//
+
+import Testing
+@testable import IPLocatorApp
+
+struct IPLocatorAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
