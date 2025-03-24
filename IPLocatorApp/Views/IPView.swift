73d89f5d9f369ac7cc089ec6559efc373bc19b56
Wasil Ahmed <ahmed.wasil@gmail.com>
2025-03-24 23:04:41 +0000

    IPView implemented with text input and buttons,

--- IPLocatorApp/Views/IPView.swift
+++ IPLocatorApp/Views/IPView.swift
@@ -5,3 +5,36 @@
 //  Created by Wasil Ahmed on 24/03/2025.
 //
 
+import SwiftUI
+import MapKit
+
+struct IPView:View {
+    @StateObject private var viewModel = IPViewModel()
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            TextField("Enter IP Address", text: $viewModel.ipAddress)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            
+            HStack {
+                Button("Get My IP") {
+                    viewModel.fetchCurrentIP()
+                }
+                .buttonStyle(.bordered)
+                
+                Button("Locate IP") {
+                    viewModel.fetchLocation(for: viewModel.ipAddress)
+                }
+                .buttonStyle(.borderedProminent)
+            }
+            
+            
+            if let errorMessage = viewModel.errorMessage {
+                Text(errorMessage)
+                    .foregroundColor(.red)
+                    .padding()
+            }
+        }
+    }
+}
