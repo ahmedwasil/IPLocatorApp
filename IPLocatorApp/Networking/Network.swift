37602124d80477f8e2641031c63fac49ce222e42
Wasil Ahmed <ahmed.wasil@gmail.com>
2025-03-24 05:24:08 +0000

    Implement NetworkService with Combine

--- IPLocatorApp/Networking/Network.swift
+++ IPLocatorApp/Networking/Network.swift
@@ -5,3 +5,27 @@
 //  Created by Wasil Ahmed on 24/03/2025.
 //
 
+import Foundation
+import Combine
+
+class NetworkService {
+    static let shared = NetworkService()
+    
+    private init() {}
+    
+    private let baseURL = "https://ipapi.co"
+    
+    func fetchIPDetails(for ip: String?) -> AnyPublisher<IPLocation, Error> {
+        let endpoint = ip.map { "\(baseURL)/\($0)/json/" } ?? "\(baseURL)/json/"
+        
+        guard let url = URL(string: endpoint) else {
+            return Fail(error: URLError(.badURL)).eraseToAnyPublisher()
+        }
+        
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map(\.data)
+            .decode(type: IPLocation.self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+}
